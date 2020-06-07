0399807610c3c28d489d32fc73d02485bb419376
trulli90 <trulli90@gmail.com>
2020-06-08 00:10:34 +0200

    Updates

---
+++ precache-manifest.884d2b34f15d08d81ce84f16e7c4fa0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4340ba19a13dd9eea9f6b1458e289aca",
+    "url": "/selfie-location-app/index.html"
+  },
+  {
+    "revision": "bcfb522a11b1c0ebf814",
+    "url": "/selfie-location-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5fc65fe4a464b1fd76b2",
+    "url": "/selfie-location-app/static/css/main.c6beb8e3.chunk.css"
+  },
+  {
+    "revision": "bcfb522a11b1c0ebf814",
+    "url": "/selfie-location-app/static/js/2.688ed450.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/selfie-location-app/static/js/2.688ed450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc65fe4a464b1fd76b2",
+    "url": "/selfie-location-app/static/js/main.39e30e48.chunk.js"
+  },
+  {
+    "revision": "e25fe4d70142d8590afb",
+    "url": "/selfie-location-app/static/js/runtime-main.920ca2c0.js"
+  }
+]);
